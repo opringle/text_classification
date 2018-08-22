e0ed0063a9a6448d7b85015f253421e68a2f397b
opringle <ojapringle@gmail.com>
2018-08-22 10:16:38 -0700

    commiting data

--- src/preprocess.py
+++ src/preprocess.py
@@ -23,6 +23,10 @@ import pandas as pd
 import logging
 import argparse
 import os
+from collections import Counter
+import itertools
+import pickle
+
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -32,6 +36,76 @@ parser.add_argument('--train-url', type=str, help='url to download csv file',
                     default='https://github.com/mhjabreel/CharCNN/raw/master/data/ag_news_csv/train.csv')
 parser.add_argument('--test-url', type=str, help='url to download csv file',
                     default='https://github.com/mhjabreel/CharCNN/raw/master/data/ag_news_csv/test.csv')
+
+
+class CharacterPreprocessor:
+    def __init__(self, char_to_index={}, index_to_char=[], intent_to_index={}, index_to_intent=[], unseen_character=-1,
+                 unseen_label=-1):
+        """
+        :param char_to_index: dictionary where key is character and value is indexed character
+        :param index_to_char: list where position is index and value is char
+        :param intent_to_index: dictionary where key is intent and value is indexed intent
+        :param index_to_intent: list where position is index and value is intent
+        :param unseen_character: value to use when indexing a character which is not in char_to_index
+        :param unseen_label: value to use when indexing an intent which is not in intent_to_index
+        """
+        self.char_to_index = char_to_index
+        self.index_to_char = index_to_char
+        self.intent_to_index = intent_to_index
+        self.index_to_intent = index_to_intent
+        self.unseen_character = unseen_character
+        self.unseen_label = unseen_label
+
+    @staticmethod
+    def split_utterance(utterance):
+        """
+        :param utterance: string
+        :return: list of string, split into characters
+        """
+        return list(utterance.lower())
+
+    @staticmethod
+    def build_vocab(data, max_vocab_size, depth=1):
+        """
+        :param data: list of data
+        :param depth: how many levels of nesting there are in list
+        :param max_vocab_size: limit vocabulary to n most popular occurrences
+        :return: dict and list mapping data to indices
+        """
+        if depth > 1:
+            data = list(itertools.chain.from_iterable(data)) # Make list 1D
+        data_counts = Counter(data)  # Count occurrences of each word in the list
+
+        vocabulary_inv = [x[0] for x in data_counts.most_common(max_vocab_size)]
+        vocabulary = {x: i for i, x in enumerate(vocabulary_inv)}
+        return vocabulary, vocabulary_inv
+
+    def fit(self, utterances, labels, max_vocab_size):
+        """
+        Build vocabulary from data
+        :param utterances: list of raw utterances
+        :param max_vocab_size: max number of characters in the vocabulary
+        :param labels: list of raw labels
+        """
+        split_utterances = [self.split_utterance(utterance) for utterance in utterances]
+        self.char_to_index, self.index_to_char = self.build_vocab(split_utterances, max_vocab_size, depth=2)
+        self.intent_to_index, self.index_to_intent = self.build_vocab(labels, max_vocab_size, depth=1)
+
+    def transform_utterance(self, utterance):
+        """
+        :param utterance: raw utterance string
+        :return: preprocessed utterance
+        """
+        split_utterance = self.split_utterance(utterance)
+        indexed_utterance = [self.char_to_index.get(token, self.unseen_character) for token in split_utterance]
+        return indexed_utterance
+
+    def transform_label(self, label):
+        """
+        :param label: raw intent label
+        :return: indexed intent label
+        """
+        return self.intent_to_index.get(label, self.unseen_label)
 
 
 def preprocess(df):
@@ -45,6 +119,25 @@ def preprocess(df):
     return df
 
 
+def index_features(train_df, test_df, preprocessor):
+    """
+    :param train_df: training data in pandas df
+    :param test_df: test data in pandas df
+    :return: indexed feature dataframes
+    """
+    # Fit prepreprocessor to training data
+    preprocessor.fit(train_df.description.tolist(), train_df.label.tolist(), max_vocab_size=200)
+
+    # Transform utterances
+    train_df['X'] = train_df['description'].apply(lambda x: preprocessor.transform_utterance(x))
+    train_df['Y'] = train_df['label'].apply(lambda x: preprocessor.transform_label(x))
+
+    test_df['X'] = test_df['description'].apply(lambda x: preprocessor.transform_utterance(x))
+    test_df['Y'] = test_df['label'].apply(lambda x: preprocessor.transform_label(x))
+
+    return train_df, test_df
+
+
 def save(df, filename):
     """
     :param df: pandas dataframe
@@ -53,6 +146,11 @@ def save(df, filename):
     if not os.path.exists(os.path.dirname(filename)):
         os.makedirs(os.path.dirname(filename))
     df.to_csv(filename, index=None)
+
+
+def save_obj(obj, name ):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == '__main__':
@@ -69,6 +167,11 @@ if __name__ == '__main__':
     train_df = preprocess(train_df)
     test_df = preprocess(test_df)
 
+    preprocessor = CharacterPreprocessor()
+    train_df, test_df = index_features(train_df, test_df, preprocessor)
+
     # Save to file
-    save(train_df, '../data/ag_news/train.csv')
-    save(test_df, '../data/ag_news/test.csv')
+    save_obj(preprocessor.char_to_index, '../data/ag_news_char/char_to_index')
+    save_obj(preprocessor.intent_to_index, '../data/ag_news_char/intent_to_index')
+    train_df.to_pickle('../data/ag_news_char/train.pickle')
+    test_df.to_pickle('../data/ag_news_char/test.pickle')
---
+++ src/train.py
@@ -0,0 +1,274 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+def load_obj(name ):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+# Read in files
+intent_to_index = load_obj('../data/ag_news_char/intent_to_index')
+char_to_index = load_obj('../data/ag_news_char/char_to_index')
+train_df = pd.read_pickle('../data/ag_news_char/train.pickle')
+test_df = pd.read_pickle('../data/ag_news_char/test.pickle')
+
+# Feature/label lists
+X_train = train_df.X.tolist()
+Y_train = train_df.Y.tolist()
+X_test = test_df.X.tolist()
+Y_test = test_df.Y.tolist()
+
+print("\nDictionary mapping characters to integer indices:\n{}\n\nDictionary mapping labels to integer indices:\n{}".format(char_to_index, intent_to_index))
+train_df.head()
+
+
+import bisect
+import random
+import numpy as np
+from mxnet.io import DataIter, DataBatch, DataDesc
+from mxnet import ndarray
+from sklearn.utils import shuffle
+
+
+class BucketUtteranceIter(DataIter):
+    """
+    This iterator can handle variable length feature arrays
+    """
+    def __init__(self, utterances, intents, batch_size, buckets, data_pad=-1, label_pad=-1, data_name='utterance',
+                 label_name='intent', dtype='float32'):
+        """
+        :param utterances: list of list of int
+        :param intents: list of int
+        """
+        super(BucketUtteranceIter, self).__init__()
+        buckets.sort()
+
+        nslice = 0  # Keep track of how many utterances are sliced
+        self.utterances = [[] for _ in buckets]
+        self.intents = [[] for _ in buckets]
+        self.indices = [[] for _ in buckets]
+
+        for i, utt in enumerate(utterances):
+            # Find the index of the smallest bucket that is larger than the sentence length
+            buck_idx = bisect.bisect_left(buckets, len(utt))
+
+            # Slice utterances that are too long to the largest bucket size
+            if buck_idx == len(buckets):
+                buck_idx = buck_idx - 1
+                nslice += 1
+                utt = utt[:buckets[buck_idx]]
+
+            # Pad utterances that are too short for their bucket
+            buff = np.full((buckets[buck_idx]), data_pad, dtype=dtype)
+            buff[:len(utt)] = utt
+
+            # Add data/label to bucket
+            self.utterances[buck_idx].append(buff)
+            self.intents[buck_idx].append(intents[i])
+            self.indices[buck_idx].append(i)
+
+        # Convert to list of array
+        self.utterances = [np.asarray(i, dtype=dtype) for i in self.utterances]
+        self.intents = [np.asarray(i, dtype=dtype) for i in self.intents]
+        self.indices = [np.asarray(i, dtype=dtype) for i in self.indices]
+
+        print("\nWarning, {0} utterances sliced to largest bucket size.".format(nslice)) if nslice > 0 else None
+        print("Utterances per bucket: {}\nBucket sizes: {}".format([arr.shape[0] for arr in self.utterances], buckets))
+
+        self.data_name = data_name
+        self.label_name = label_name
+        self.batch_size = batch_size
+        self.buckets = buckets
+        self.dtype = dtype
+        self.data_pad = data_pad
+        self.label_pad = label_pad
+        self.default_bucket_key = max(buckets)
+        self.layout = 'NT'
+
+        self.provide_data = [DataDesc(name=self.data_name,
+                                      shape=(self.batch_size, self.default_bucket_key),
+                                      layout=self.layout)]
+        self.provide_label = [DataDesc(name=self.label_name,
+                                       shape=(self.batch_size, ),
+                                       layout=self.layout)]
+
+        # create empty list to store batch index values
+        self.idx = []
+        for i, buck in enumerate(self.utterances):
+            self.idx.extend([(i, j) for j in range(0, len(buck) - batch_size + 1, batch_size)])
+        self.curr_idx = 0
+        self.reset()
+
+    def reset(self):
+        """
+        Resets the iterator to the beginning of the data.
+        """
+        self.curr_idx = 0
+        # shuffle data in each bucket
+        random.shuffle(self.idx)
+        for i, buck in enumerate(self.utterances):
+            self.indices[i], self.utterances[i], self.intents[i] = shuffle(self.indices[i],
+                                                                           self.utterances[i],
+                                                                           self.intents[i])
+        self.ndindex = []
+        self.ndsent = []
+        self.ndlabel = []
+
+        # append the lists with an array
+        for i, buck in enumerate(self.utterances):
+            self.ndindex.append(ndarray.array(self.indices[i], dtype=self.dtype))
+            self.ndsent.append(ndarray.array(self.utterances[i], dtype=self.dtype))
+            self.ndlabel.append(ndarray.array(self.intents[i], dtype=self.dtype))
+
+    def next(self):
+        """
+        Returns the next batch of data.
+        """
+        if self.curr_idx == len(self.idx):
+            raise StopIteration
+        # i = batches index, j = starting record
+        i, j = self.idx[self.curr_idx]
+        self.curr_idx += 1
+
+        indices = self.ndindex[i][j:j + self.batch_size]
+        utterances = self.ndsent[i][j:j + self.batch_size]
+        intents = self.ndlabel[i][j:j + self.batch_size]
+
+        return DataBatch([utterances],
+                         [intents],
+                         pad=0,
+                         index=indices,
+                         bucket_key=self.buckets[i],
+                         provide_data=[DataDesc(name=self.data_name, shape=utterances.shape, layout=self.layout)],
+                         provide_label=[DataDesc(name=self.label_name, shape=intents.shape, layout=self.layout)])
+
+
+batch_size=12
+
+train_iter = BucketUtteranceIter(X_train, Y_train, batch_size, buckets=[32,64,128,256,512,1024])
+test_iter = BucketUtteranceIter(X_test, Y_test, batch_size, buckets=[32,64,128,256,512, 1024])
+
+for i, batch in enumerate(train_iter):
+    if i < 1:
+        print("\nBatch {} Bucket size {}\nData\n {} \nLabel\n {}\n".format(i, batch.bucket_key, batch.data, batch.label))
+train_iter.reset()
+
+import mxnet as mx
+
+
+def bucketed_module(train_iter, vocab_size, dropout, num_label, smooth_alpha, default_bucket_key, context):
+    """
+    :param train_iter:
+    :param vocab_size:
+    :param dropout:
+    :param num_label:
+    :param smooth_alpha:
+    :param default_bucket_key:
+    :param context:
+    :return:
+    """
+
+    def sym_gen(seq_len):
+        """
+        :param seq_len: bucket size
+        :return: symbol for neural network architecture
+        """
+
+        def conv(data, num_filter, kernel=(1, 1), stride=(1, 1), pad=(0, 0), name=None, suffix=''):
+            conv = mx.sym.Convolution(data=data, num_filter=num_filter, kernel=kernel, stride=stride, pad=pad,
+                                      no_bias=True, name='%s%s_conv2d' % (name, suffix))
+            bn = mx.sym.BatchNorm(data=conv, name='%s%s_batchnorm' % (name, suffix), fix_gamma=True)
+            act = mx.sym.Activation(data=bn, act_type='relu', name='%s%s_relu' % (name, suffix))
+            return act
+
+        def conv_block(data, num_filter, name):
+            conv1 = conv(data, kernel=(1, 3), num_filter=num_filter, pad=(0, 1), name='conv1' + str(name))
+            conv2 = conv(conv1, kernel=(1, 3), num_filter=num_filter, pad=(0, 1), name='conv2' + str(name))
+            return conv2
+
+        X_shape = (train_iter.batch_size, seq_len)
+        Y_shape = (train_iter.batch_size,)
+
+        print("\nNetwork architecture for bucket size {}\n".format(seq_len))
+
+        data = mx.sym.Variable(name="utterance")
+        softmax_label = mx.sym.Variable(name="intent")
+        print("data_input: ", data.infer_shape(utterance=X_shape)[1][0])
+        print("label input: ", softmax_label.infer_shape(intent=Y_shape)[1][0])
+
+        # Embed each character to 16 channels
+        embedded_data = mx.sym.Embedding(data, input_dim=vocab_size, output_dim=16)
+        embedded_data = mx.sym.Reshape(mx.sym.transpose(embedded_data, axes=(0, 2, 1)), shape=(0, 0, 1, -1))
+        print("embed layer output shape: ", embedded_data.infer_shape(utterance=X_shape)[1][0])
+
+        # Temporal Convolutional Layer (without activation)
+        temp_conv = mx.sym.Convolution(embedded_data, kernel=(1, 3), num_filter=64, pad=(0, 1))
+        print("Temp conv output shape: ", temp_conv.infer_shape(utterance=X_shape)[1][0])
+
+        # Create convolutional blocks with pooling in-between
+        block = conv_block(temp_conv, num_filter=64, name='block1_1')
+        block = conv_block(block, num_filter=64, name='block1_2')
+        pool = mx.sym.Pooling(block, kernel=(1, 3), stride=(1, 2), pad=(0, 1), pool_type='max')
+        print("Block 1 output shape: {}".format(pool.infer_shape(utterance=X_shape)[1][0]))
+
+        block = conv_block(pool, num_filter=128, name='block2_1')
+        block = conv_block(block, num_filter=128, name='block2_2')
+        pool = mx.sym.Pooling(block, kernel=(1, 3), stride=(1, 2), pad=(0, 1), pool_type='max')
+        print("Block 2 output shape: {}".format(pool.infer_shape(utterance=X_shape)[1][0]))
+
+        block = conv_block(pool, num_filter=256, name='block3_1')
+        block = conv_block(block, num_filter=256, name='block3_2')
+        pool = mx.sym.Pooling(block, kernel=(1, 3), stride=(1, 2), pad=(0, 1), pool_type='max')
+        print("Block 3 output shape: {}".format(pool.infer_shape(utterance=X_shape)[1][0]))
+
+        block = conv_block(pool, num_filter=512, name='block4_1')
+        block = conv_block(block, num_filter=512, name='block4_2')
+        print("Block 4 output shape: {}".format(block.infer_shape(utterance=X_shape)[1][0]))
+
+        pool_k = seq_len // 8
+        print("{0} pool kernel size {1}, stride 1".format('avg', pool_k))
+        block = mx.sym.flatten(mx.sym.Pooling(block, kernel=(1, pool_k), stride=(1, 1), pad=(0, 0), pool_type='avg'))
+        print("flattened pooling output shape: {}".format(block.infer_shape(utterance=X_shape)[1][0]))
+        block = mx.sym.Dropout(block, p=dropout)
+        print("dropout layer output shape: {}".format(block.infer_shape(utterance=X_shape)[1][0]))
+
+        output = mx.sym.FullyConnected(block, num_hidden=num_label, flatten=True, name='output')
+        sm = mx.sym.SoftmaxOutput(output, softmax_label, smooth_alpha)
+        print("softmax output shape: {}".format(sm.infer_shape(utterance=X_shape)[1][0]))
+
+        return sm, ('utterance',), ('intent',)
+
+    return mx.mod.BucketingModule(sym_gen=sym_gen, default_bucket_key=default_bucket_key, context=context)
+
+
+
+module = bucketed_module(train_iter,
+                         vocab_size=150,
+                         dropout=0.02,
+                         num_label=4,
+                         smooth_alpha=0.004,
+                         default_bucket_key=train_iter.default_bucket_key,
+                         context=mx.cpu())
+
+
+
+# Reduce learning rate every 3 epochs
+batches_per_epoch = int(sum([len(bucket) for bucket in train_iter.ndsent])/batch_size)
+step = 3 * batches_per_epoch
+schedule = mx.lr_scheduler.FactorScheduler(step=step, factor=0.97)
+
+# Initialize convolutional filter weights using MSRAPRelu to aid training deeper architectures
+init = mx.initializer.Mixed(patterns=['conv2d_weight', '.*'],
+                            initializers=[mx.initializer.MSRAPrelu(factor_type='avg', slope=0.25),
+                                          mx.initializer.Normal(sigma=0.02)])
+
+# Learn network weights from data
+module.fit(train_data=train_iter,
+           eval_data=test_iter,
+           eval_metric=mx.metric.Accuracy(),
+           optimizer='sgd',
+           optimizer_params={'learning_rate': 0.06,
+                             'momentum': 0.93,
+                             'lr_scheduler': schedule},
+           # initializer=init,
+           num_epoch=30)
